b054b643dab294572087097cfcd6194732417c0f
Daniel Macharia <danielhuios@gmail.com>
2022-10-16 05:42:40 +0300

    first ruby lesson

---
+++ app.rb
@@ -0,0 +1,17 @@
+# puts is a built-in Ruby method that will output a string of text to the terminal. It's the Ruby equivalent of console.log() in JavaScript
+puts "Hello World!"
+
+print "Pass this test, please."
+
+# puts [1, 2, 3]
+
+# #puts .inspect == p
+# puts [1, 2, 3].inspect
+p [1, 2, 3]
+
+# # pretty printing pp
+# puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# p [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# #the puts method always returns nil
